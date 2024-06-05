672c785566226562a39b5cd7affa3f7c81f18b63
Jhair Alejandro Cruz Palacios <223244@ids.upchiapas.edu.mx>
2024-06-04 21:18:13 -0600

    firs commit

---
+++ src/app/login/page.tsx
@@ -0,0 +1,13 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Login Page",
+  description: "Login Page",
+};
+
+export default function Login() {
+    return (
+        <>
+            <h1>Login</h1>
+        </>
+    )}
