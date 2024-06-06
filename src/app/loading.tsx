238571aa75e828c885db092137643c2568e58a18
Jhair Alejandro Cruz Palacios <223244@ids.upchiapas.edu.mx>
2024-06-06 00:09:26 -0600

    loafing implementado

---
+++ src/app/loading.tsx
@@ -0,0 +1,10 @@
+import { LoadingComp } from "@/components/loading/LoadingComp";
+
+
+export default function Loading() {
+  return (
+    <div>
+        <LoadingComp />
+    </div>
+  );
+}
---
+++ src/components/loading/index.ts
@@ -0,0 +1,1 @@
+import { LoadingComp } from './LoadingComp';
