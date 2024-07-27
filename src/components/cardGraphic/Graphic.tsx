4ac979f86872d1d45d4d2901bbfece61e7bad9d1
Jhair Alejandro Cruz Palacios <223244@ids.upchiapas.edu.mx>
2024-07-26 23:54:22 -0600

    vists agregadas

--- src/components/cardGraphic/Graphic.tsx
+++ src/components/cardGraphic/Graphic.tsx
@@ -3,11 +3,10 @@ import Link from "next/link"
 export const Graphic = () => {
   return (
     <div className="rounded-lg bg-white p-6 shadow-md text-black">
-        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam quae fuga perferendis esse quas adipisci dicta aliquam velit culpa ipsum, quod officia ratione animi. Vitae animi reiciendis esse vel aliquid.</h1>
-        <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis excepturi nesciunt ea officiis voluptatum repudiandae, vero maxime ducimus inventore vel placeat maiores libero odio amet distinctio, quam numquam. Obcaecati, accusamus?</h2>  
-        <h3>josep puto</h3>        
+      <Link href="/graphics" className="text-[#333] hover:text-[#FFA500]">
+                  ver
+        </Link>
     </div>
   )
 }
-
 
