51a02da61541db1315ffffe9e5ab4da2a24812af
Jhair Alejandro Cruz Palacios <223244@ids.upchiapas.edu.mx>
2024-07-26 23:49:49 -0600

    vista para josep impleentada

---
+++ src/app/graphics/page.tsx
@@ -0,0 +1,15 @@
+import { Footer } from "@/components/footer/Footer"
+import { Header } from "@/components/header/Header"
+
+
+export default function Graphics() {
+    return (
+        <div className="flex min-h-screen flex-col bg-gradient-to-r from-[#FF6B6B] to-[#FFA500]">
+            <Header />
+                <h1>vista josep</h1>
+            
+
+            <Footer />
+        </div>
+    )
+}
